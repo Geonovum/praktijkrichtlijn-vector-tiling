f5e846f9447a40395dfa59e4b7ef6be29b1e2955
Thijs Brentjens <thijs@brentjensgeoict.nl>
2021-08-30 11:48:56 +0200

    fix previousMaturity

--- config.js
+++ config.js
@@ -97,7 +97,7 @@ var respecConfig =
 
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit!
   previousPublishDate: "2021-04-15",  	    // Format is "YYY-MM-DD"
-  previousMaturity: "CV",                   // kies 1 van deze 2 regels
+  previousMaturity: "GN-CV",                   // kies 1 van deze 2 regels
   //previousMaturity: "VV",  	                // kies 1 van deze 2 regels
 
   //-- Optionele parameters:
